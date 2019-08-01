f9b97bf7c58ae573a85f46c62992b92694474331
victorfouquet <victor.fouquet@gmail.com>
2019-08-01 14:22:37 +0200

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,32 @@
 Doctor.destroy_all
 Patient.destroy_all
 Appointment.destroy_all
+City.destroy_all
+cities = ["Paris", "Nantes", "Rennes", "Strasbourg", "Bordeaux", "Rouen", "Lille", "Tours", "Marseille", "Reims"]
+n = 0
+
+10.times do
+  city = City.create!(
+    name: cities[n] 
+    )
+  n += 1
+end
+
 100.times do
   doctor = Doctor.create!(
   	first_name: Faker::Name.first_name,
   	last_name: Faker::Name.last_name,
   	speciality: Faker::Job.field,
-  	zip_code: Faker::Number.number(digits: 5).to_s
+  	zip_code: Faker::Number.number(digits: 5).to_s,
+    city: City.find(rand(City.first.id..City.last.id))
     )
 end
 
 100.times do
 	patient = Patient.create!(
   	first_name: Faker::Name.first_name,
-  	last_name: Faker::Name.last_name
+  	last_name: Faker::Name.last_name,
+    city: City.find(rand(City.first.id..City.last.id))
   	)
 end
 
@@ -28,6 +41,7 @@ end
 	appointment = Appointment.create!(
   	date: Faker::Time.forward(days: 5,  period: :day, format: :long),
   	doctor: Doctor.find(rand(Doctor.first.id..Doctor.last.id)),
-  	patient: Patient.find(rand(Patient.first.id..Patient.last.id))
+  	patient: Patient.find(rand(Patient.first.id..Patient.last.id)),
+    city: City.find(rand(City.first.id..City.last.id))
   	)
 end
